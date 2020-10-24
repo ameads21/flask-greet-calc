38fe4bdbaeeb64c7d996599df53533abf2fbc16d
Aaron Meads <aaron.meads@gmail.com>
2020-10-24 16:55:00 -0600

    Adding information in the app.py folder in calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,52 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def result_add():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = add(a,b)
+  return str(result)
+
+
+
+@app.route('/sub')
+def result_sub():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = sub(a,b)
+  return str(result)
+
+
+@app.route('/mult')
+def result_mult():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = mult(a,b)
+  return str(result)
+
+
+@app.route('/div')
+def result_div():
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = div(a,b)
+  return str(result)
+
+
+
+operators = {
+  "add": add,
+  "sub": sub,
+  "mult": mult,
+  "div": div,
+}
+
+@app.route('/math/<oper>')
+def math_results(oper):
+  a = int(request.args.get("a"))
+  b = int(request.args.get("b"))
+  result = operators[oper](a,b)
+  return str(result)
